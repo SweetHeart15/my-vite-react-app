691e575e7d3c267db4f157a0b1a3492146d1fcaf
SweetHeart15 <kristineheartm@gmail.com>
2026-05-16 01:59:30 +0800

    removed local support

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Transactions.css';
 
-const API = 'http://localhost:8000';
+const API = import.meta.env.VITE_API_URL;
 
 const categoryIcon = (cat) => ({
   'Dining & Drinks': '☕',
